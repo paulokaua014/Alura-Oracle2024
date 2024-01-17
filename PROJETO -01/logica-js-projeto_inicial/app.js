8b299aadca72fdf376599b5948a3872bd1775745
paulokauã <kpaulopp@gmail.com>
2024-01-17 15:29:18 -0300

    modificaçao no codigo adicionado o tentativas no progama

--- PROJETO -01/logica-js-projeto_inicial/app.js
+++ PROJETO -01/logica-js-projeto_inicial/app.js
@@ -2,15 +2,18 @@ alert('Baos Vindas ao jogo do numero secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
+
 while (chute!= numeroSecreto) {
     chute = prompt('Escolha o numero de 1 a 100');
     if(chute == numeroSecreto){
-        alert('isso ai!! voce acertou o numero secreto');
+        alert('isso ai!! voce acertou o numero secreto com tantas ' +tentativas);
     }else{
         if (chute > numeroSecreto) {
-            alert('Numero secreto e menor '+chute)
+            alert('Numero secreto e menor '+chute);
         }else{
         alert('Numero secreto e maior '+chute);
         }
     };
+    tentativas++;
 }
