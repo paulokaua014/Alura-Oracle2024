52127d7f29e76da92ef47e722bcc1dd47538a238
paulokauã <kpaulopp@gmail.com>
2024-01-17 15:22:03 -0300

    modificaçao no codigo adicionado o while

--- PROJETO -01/logica-js-projeto_inicial/app.js
+++ PROJETO -01/logica-js-projeto_inicial/app.js
@@ -1,13 +1,16 @@
 alert('Baos Vindas ao jogo do numero secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha o numero de 1 a 100');
-if(chute == numeroSecreto){
-    alert('isso ai!! voce acertou o numero secreto');
-}else{
-    if (chute > numeroSecreto) {
-        alert('Numero secreto e menor '+chute)
+let chute;
+while (chute!= numeroSecreto) {
+    chute = prompt('Escolha o numero de 1 a 100');
+    if(chute == numeroSecreto){
+        alert('isso ai!! voce acertou o numero secreto');
     }else{
-    alert('Numero secreto e maior '+chute);
-    }
-};
+        if (chute > numeroSecreto) {
+            alert('Numero secreto e menor '+chute)
+        }else{
+        alert('Numero secreto e maior '+chute);
+        }
+    };
+}
