1350176a49ba145d12e39cd4b34ba57df4d027a3
paulokauã <kpaulopp@gmail.com>
2024-01-17 15:09:33 -0300

    modificaçao no codigo

--- PROJETO -01/logica-js-projeto_inicial/app.js
+++ PROJETO -01/logica-js-projeto_inicial/app.js
@@ -1,9 +1,13 @@
 alert('Baos Vindas ao jogo do numero secreto');
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute = prompt('Escolha o numero de 1 a 100');
-if(numeroSecreto == chute){
+if(chute == numeroSecreto){
     alert('isso ai!! voce acertou o numero secreto');
 }else{
-    alert('voce errou, o numero secreto e '+numeroSecreto);
+    if (chute > numeroSecreto) {
+        alert('Numero secreto e menor '+chute)
+    }else{
+    alert('Numero secreto e maior '+chute);
+    }
 };
