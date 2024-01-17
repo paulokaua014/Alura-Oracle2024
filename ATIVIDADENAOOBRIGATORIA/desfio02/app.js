bc5ef62471b4a4919a9a1df730502003ab8f4a62
paulokauã <kpaulopp@gmail.com>
2024-01-17 14:50:45 -0300

    desafio 02

---
+++ ATIVIDADENAOOBRIGATORIA/desfio02/app.js
@@ -0,0 +1,8 @@
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
