2770edca9c595953cd1d550edf59523e5656d833
paulokauã <kpaulopp@gmail.com>
2024-01-18 16:15:38 -0300

    Alteracao do codigo adicionando o parseint e o math random no codigo

--- PROJETO -01/logica-js-projeto_inicial/app.js
+++ PROJETO -01/logica-js-projeto_inicial/app.js
@@ -1,11 +1,11 @@
-alert('Baos Vindas ao jogo do numero secreto');
-let numeroSecreto = 5;
+alert('Boas Vindas ao jogo do numero secreto');
+let numeroSecreto =parseInt(Math.random()* 10+1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute!= numeroSecreto) {
-    chute = prompt('Escolha o numero de 1 a 100');
+    chute = prompt('Escolha o numero de 1 a 10');
     if(chute == numeroSecreto){
         break;
     }else{
