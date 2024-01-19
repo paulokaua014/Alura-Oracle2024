1db2a5b716cd9dc68886fc06159a87b2b7d60eb2
paulokauã <kpaulopp@gmail.com>
2024-01-19 11:59:26 -0300

    Adicionando e aprendendo a funcao no js e adicionado no jogo

--- PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do Numero Secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute() {
+    console.log('Esse butao foi clicado')    
+}
