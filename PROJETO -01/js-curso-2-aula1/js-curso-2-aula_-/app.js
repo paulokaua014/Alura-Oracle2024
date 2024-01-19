ef8fa68b47f3dc08011c77a8993652d72305d436
paulokauã <kpaulopp@gmail.com>
2024-01-19 15:13:53 -0300

    modificando o codigo adicionando lista para nao repetir numeros repitidos

--- PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -33,8 +35,17 @@ function verificarChute() {
     
 };
 function gerarNumeroAleatorio() {
-   return parseInt(Math.random() * 10 + 1);
-    
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNalista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+        }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 function limparCampo() {
     let chute = document.querySelector('input');
