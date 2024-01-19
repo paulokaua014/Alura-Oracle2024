a8b1370ab006f64c695115b6e471eec62816686c
paulokauã <kpaulopp@gmail.com>
2024-01-19 13:57:21 -0300

    modificando o codigo pra ter o jogo mais bonito

--- PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -5,11 +5,20 @@ function exiberTextoNaTela(tag, texto) {
     campo.innerHTML = texto;    
 }
 exiberTextoNaTela('h1', 'Jogo do Numero Secreto')
-exiberTextoNaTela('p', 'Escolha um numero entre 1 e 100')
+exiberTextoNaTela('p', 'Escolha um numero entre 1 e 10')
 
 function verificarChute() {
     let chute = document.querySelector('input').value
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        exiberTextoNaTela('h1', 'Acertou!!');
+        exiberTextoNaTela('p','Parabens por acertar o numero secreto');
+    }else{
+    if (chute > numeroSecreto) {
+        exiberTextoNaTela('p','O numero secreto e menor!!');
+    }else{
+        exiberTextoNaTela('p', 'O numero secreto e maior!!');
+    }
+    }
 };
 function gerarNumeroAleatorio() {
    return parseInt(Math.random() * 10 + 1);
