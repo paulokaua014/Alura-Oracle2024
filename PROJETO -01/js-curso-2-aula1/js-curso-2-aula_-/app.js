5e951fff2b85f6bc8e2175557b211d4393f1a22a
paulokauã <kpaulopp@gmail.com>
2024-01-19 11:50:51 -0300

    Adicionando o projeto 2 do curso de logica de progamaçao e adicionado titulo e paragrafo com js

---
+++ PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Numero Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
