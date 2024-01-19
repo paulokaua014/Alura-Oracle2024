c7ba63f4c4e0034a91612c6b5e2756616a1ae487
paulokauã <kpaulopp@gmail.com>
2024-01-19 13:26:12 -0300

    conclusao do desafio 09

---
+++ ATIVIDADENAOOBRIGATORIA/desafio09/app.js
@@ -0,0 +1,38 @@
+function mensagem() {
+    console.log("Ola mundo")
+}
+mensagem();
+function exibirOlaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+  
+exibirOlaNome("Paulo");
+
+function calcularDobro(numero) {
+    return numero * 2;
+}
+  
+let resultadoDobro = calcularDobro(5);
+console.log(resultadoDobro);
+
+function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+}
+  
+let media = calcularMedia(4, 7, 10);
+
+console.log(media);
+
+  function encontrarMaior(a, b) {
+    return a > b ? a : b;
+}
+let maiorNumero = encontrarMaior(15, 9);
+console.log(maiorNumero);
+
+function quadrado(numero) {
+    return numero * numero;
+}
+  
+let resultado = quadrado(2);
+console.log(resultado); 
+
