429688d8fa74600b7c5c75db9bf0f0630703bfa7
paulokauã <kpaulopp@gmail.com>
2024-01-19 15:24:11 -0300

    desafio numero 10 feito com sucesso

---
+++ ATIVIDADENAOOBRIGATORIA/desafio10/app.js
@@ -0,0 +1,14 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+let linguagensDeProgramacao1 = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao1.push('Java', 'Ruby','GoLang');
+console.log(linguagensDeProgramacao1);
+
+let nomesaletorios = ['paulo','vasco','bernado'];
+console.log(nomesaletorios[0]);
+
+let nomesaletorios1 = ['paulo','vasco','bernado'];
+console.log(nomesaletorios1[1]);
+let nomesaletorios2 = ['paulo','vasco','bernado'];
+console.log(nomesaletorios2[2]);
