5d3e1f4466ae4010eb9adbc560cfe7f3481dcd06
paulokauã <kpaulopp@gmail.com>
2024-01-19 12:48:18 -0300

    modificando e aprendendo os parametros da funçao na js

--- PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ PROJETO -01/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,9 +1,10 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Numero Secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+function exiberTextoNaTela(tag, texto) {
+    let = campo = document.querySelector(tag);
+    campo.innerHTML = texto;    
+}
+exiberTextoNaTela('h1', 'Jogo do Numero Secreto')
+exiberTextoNaTela('p', 'Escolha um numero entre 1 e 100')
 
 function verificarChute() {
-    console.log('Esse butao foi clicado')    
-}
+    console.log('Esse butao foi clicado')    ;
+};
